f16e2598a8363c96d33565ae1d2571fd54134e0c
Steve Lipton <stevelipton@brk-16892.local>
2018-02-12 06:39:41 -0800

    Initial Commit

---
+++ ImageLiterals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageLiterals
+//
+//  Created by Steve Lipton on 2/12/18.
+//  Copyright © 2018 Steve Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
